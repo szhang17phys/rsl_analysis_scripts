4ca6165c2cdd527795170582d46008de317759be
Shuaixiang Zhang <shuaixiangzhang@Shuaixiangs-MacBook-Air.local>
2024-04-22 14:38:45 -0400

    pmt1 and pmt2 compared

--- slice_fit_p1.cxx
+++ slice_fit_p1.cxx
@@ -707,10 +707,10 @@ void slice_fitTMP(const std::string& rsl){
 //Main function!============================================
 void slice_fit_p1(){
 
-    slice_fitTMP("rsl99");
+//    slice_fitTMP("rsl99");
 //    slice_fitTMP("rsl50");
 //    slice_fitTMP("rsl70");
-//    slice_fitTMP("rsl70");
+    slice_fitTMP("rsl130");
 //    slice_fitTMP("rsl150");    
 
 }
