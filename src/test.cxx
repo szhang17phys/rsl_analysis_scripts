ae2b3d5a029988c8d1c5be140c4bdb53780dd62b
Shuaixiang Zhang <shuaixiangzhang@Shuaixiangs-MacBook-Air.local>
2024-04-27 18:26:00 -0400

    new distance added

--- src/test.cxx
+++ src/test.cxx
@@ -15,12 +15,12 @@
 using namespace std;
 
 void test(){
-    Point3D v1 = {200, -417, 149};
+    Point3D v1 = {302, -417, 149};
     Point3D v2 = {380, 588, 130};
     Point3D v3 = {-400, -588, 100};
 
 
-    cout<<"\nAverage Solid angle: "<< solid_angle(v1, v2, v3)<<endl;
-
+    cout<<"\nAverage Solid angle: "<< distance_point_line(v1, v2, v3)<<endl;
+    cout<<"\nAverage Solid angle: "<< solid_angle2(v1, v2, v3)<<endl;
 
 }
