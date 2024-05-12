928e4521321d0d42d7340b24dd34bece9bef675f
Shuaixiang Zhang <shuaixiangzhang@Shuaixiangs-MacBook-Air.local>
2024-05-11 21:27:46 -0400

    rsl130 updated

--- slice_fit_p1.cxx
+++ slice_fit_p1.cxx
@@ -709,10 +709,10 @@ void slice_fitTMP(const std::string& rsl){
 void slice_fit_p1(){
 
 //    slice_fitTMP("rsl99");
-    slice_fitTMP("rsl50");
-    slice_fitTMP("rsl70");
+//    slice_fitTMP("rsl50");
+//    slice_fitTMP("rsl70");
     slice_fitTMP("rsl130");
-    slice_fitTMP("rsl150");     
+//    slice_fitTMP("rsl150");     
 
 }
 //=========================================================
